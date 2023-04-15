5f5761723fe10775d02414b1bbbe4b021e5f9335
Jay Pankey <pankey.jay@gmail.com>
2023-04-15 15:30:58 -0500

    Fixed dropout model variable name

--- main.py
+++ main.py
@@ -5,42 +5,42 @@
 
 # Link to MNIST tutorial: https://github.com/wxs/keras-mnist-tutorial/blob/master/MNIST%20in%20Keras.ipynb
 # Link to dropout turorial
-import tensorflow as tf;
-import numpy as np;
-from keras.datasets import mnist;
-from keras.models import Sequential;
-from keras.layers.core import Dropout, Activation, Dense;
-from keras.utils import np_utils;
+import tensorflow as tf
+import numpy as np
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers.core import Dropout, Activation, Dense
+from keras.utils import np_utils
 
 #10 different numbers.
-num_classes = 10;
+num_classes = 10
 #Sets the dropout ratio.
-dropout_prob = 0.2;
+dropout_prob = 0.2
 
 #loads in the dataset.
-(x_train, y_train), (x_test, y_test) = mnist.load_data();
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 #Reshapes the images from 28x28 to 784 array
-x_train = x_train.reshape(60000, 784);
-x_test = x_test.reshape(10000, 784);
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
 
 
 # For preprocssing MNIST: https://www.kaggle.com/code/damienbeneschi/mnist-eda-preprocessing-classifiers/notebook
 
 
 #Changes imput from scalar RGB value to binary White or Black
-x_train = x_train / 255;
-x_test = x_test / 255;
+x_train = x_train / 255
+x_test = x_test / 255
 #Remove Pixles of constant intensity to process faster
 
     
 
 def runDropout():
-    dropout_model = Sequential();
-    model.add(Dense(4096, input_shape=(784,), Activation = "relu"));
-    model.add(Dropout(dropout_prob));
-    model.add(Dense(4096, input_shape=(784,), Activation = "relu"));
-    model.add(Dropout(dropout_prob));
+    dropout_model = Sequential()
+    dropout_model.add(Dense(4096, input_shape=(784,), Activation="relu"))
+    dropout_model.add(Dropout(dropout_prob))
+    dropout_model.add(Dense(4096, input_shape=(784,), Activation="relu"))
+    dropout_model.add(Dropout(dropout_prob))
 
 
 def print_hi(name):
